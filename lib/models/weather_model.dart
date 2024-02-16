69da98d940203ee334070fe4cdbbc7c6ef47b8b3
RacimFethallah <RacimFethallah@proton.me>
2024-02-16 16:45:30 +0100

    Mise à jour de la méthode Weather.fromJson dans weather_model.dart

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -13,15 +13,15 @@ class Weather {
       required this.mainCondition});
 
   factory Weather.fromJson(Map<String, dynamic> json) {
-  return Weather(
-    city: json['name'] ?? '',
-    // latitude: json['coord'] != null ? (json['coord']['lat'] as num).toDouble() : 0.0,
-    // longitude: json['coord'] != null ? (json['coord']['lon'] as num).toDouble() : 0.0,
-    temperature: json['main'] != null ? (json['main']['temp'] as num).toDouble() : 0.0,
-    mainCondition: json['weather'] != null && json['weather'].isNotEmpty
-        ? json['weather'][0]['main'].toString()
-        : '',
-  );
-}
-
+    return Weather(
+      city: json['name'] ?? '',
+      // latitude: json['coord'] != null ? (json['coord']['lat'] as num).toDouble() : 0.0,
+      // longitude: json['coord'] != null ? (json['coord']['lon'] as num).toDouble() : 0.0,
+      temperature:
+          json['main'] != null ? (json['main']['temp'] as num).toDouble() : 0.0,
+      mainCondition: json['weather'] != null && json['weather'].isNotEmpty
+          ? json['weather'][0]['main'].toString()
+          : '',
+    );
+  }
 }
